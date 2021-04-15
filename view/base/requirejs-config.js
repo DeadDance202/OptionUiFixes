8aa813303c448171f05b0534ce0247fc62a88b12
Alexandr Gamayunov <deaddance202@gmail.com>
2021-04-15 17:25:49 +0300

    compatible with APO - 2.33.4

---
+++ view/base/requirejs-config.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright © MageWorx. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
+            optionBase: 'MageWorx_OptionUiFixes/js/catalog/product/base',
+            optionAdvancedPricing: 'MageWorx_OptionUiFixes/js/catalog/product/advanced-pricing',
+            optionDependency: 'MageWorx_OptionUiFixes/js/catalog/product/dependency',
+            optionFeatures: 'MageWorx_OptionUiFixes/js/catalog/product/features',
+            optionFeaturesIsDefault: 'MageWorx_OptionUiFixes/js/catalog/product/isDefault',
+            selectionLimitValidationRule: 'MageWorx_OptionUiFixes/js/catalog/product/selectionLimitValidationRule',
+            optionAdditionalImages: 'MageWorx_OptionUiFixes/js/catalog/product/additional',
+            mwImageReplacer: 'MageWorx_OptionUiFixes/js/catalog/product/imageReplacer',
+            optionInventory: 'MageWorx_OptionUiFixes/js/catalog/product/inventory',
+            optionSwatches: 'MageWorx_OptionUiFixes/js/catalog/product/swatches',
+            dynamicOptions: 'MageWorx_OptionUiFixes/js/catalog/product/dynamicOptions',
+            dynamicOptionMinValueValidationRule: 'MageWorx_OptionUiFixes/js/catalog/product/dynamicOptionMinValueValidationRule',
+            dynamicOptionMaxValueValidationRule: 'MageWorx_OptionUiFixes/js/catalog/product/dynamicOptionMaxValueValidationRule',
+            dynamicOptionStepValidationRule: 'MageWorx_OptionUiFixes/js/catalog/product/dynamicOptionStepValidationRule'
+        }
+    }
+};
